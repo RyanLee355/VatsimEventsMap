de9fdf780c4956fb3c19fa00732fbbf3b83dbcd7
unknown <miggle.355@gmail.com>
2026-01-28 22:12:30 +0000

    Fix top white margin

--- app/page.tsx
+++ app/page.tsx
@@ -294,7 +294,7 @@ export default function Home() {
         }));
 
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black border" style={{fontFamily: 'monospace'}}>
+        <div style={{fontFamily: 'monospace'}}>
             <main>
                 <GlobeComponent routes={filteredRoutes} rings={filteredRings} airportPoints={airportPoints} />
             </main>
